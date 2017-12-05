6ee57ebc3dddceccf1027de1316bbe7fc8871ad5
Jimmy <chen.jimmym@gmail.com>
2017-12-04 16:07:25 -0800

    part 2 done

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -3,4 +3,24 @@ class DojosController < ApplicationController
         @dojos = Dojo.all
         render "index"
     end
+    def new
+        render "new"
+    end
+    def create
+        create = Dojo.create(dojo_params)
+        if create.id == nil
+            flash[:error] = "All fields must be filled!"
+            redirect_to "/dojos/new"
+        else
+            flash[:success] = "New Dojo Added!"
+            redirect_to "/dojos"
+        end
+    end
+
+
+    private
+        def dojo_params
+            params.require(:dojo).permit(:branch, :street, :city, :state)
+        end
+
 end
